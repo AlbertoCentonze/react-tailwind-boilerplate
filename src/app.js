ba109754c5e8008f2156a15ae2d5f76cbf18bdbe
Alberto <albicento.ac@gmail.com>
2021-09-22 14:19:25 +0200

    Added tailwind CSS

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import React from "react"
 import { render } from "react-dom"
+import "./style.css"
 
-render(<h1>Hi!</h1>, document.getElementById("app-root"))
+render(<h1 className="text-4xl">Hi there!</h1>, document.getElementById("app-root"))
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,24 @@
+const path = require("path")
+
 module.exports = {
-  entry: './src/app',
+  mode: "production",
+  entry: "./src/app.js",
   output: {
-    filename: 'bundle.js'
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: 'babel-loader'
-      }
-    ]
-  }
-};
+        use: "babel-loader",
+			},
+			{
+				test: /\.css$/i,
+				include: path.resolve(__dirname, "src"),
+				use: ["style-loader", "css-loader", "postcss-loader"],
+			}
+    ],
+  },
+}
