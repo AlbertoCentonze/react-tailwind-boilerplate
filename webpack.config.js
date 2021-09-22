6b31c24eba26e763aa4d917cc753f2976aa7d8c3
Alberto <albicento.ac@gmail.com>
2021-09-22 14:52:46 +0200

    Added index.html to build

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin")
 const path = require("path")
 
 module.exports = {
@@ -7,18 +8,23 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public/index.html"),
+    }),
+  ],
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: "babel-loader",
-			},
-			{
-				test: /\.css$/i,
-				include: path.resolve(__dirname, "src"),
-				use: ["style-loader", "css-loader", "postcss-loader"],
-			}
+      },
+      {
+        test: /\.css$/i,
+        include: path.resolve(__dirname, "src"),
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
     ],
   },
 }
